4a6de156f20a39a1c65c2e91a6d31f23cbb806d3
samlaudev <liuyaozhu13hao@163.com>
2016-09-28 17:47:56 +0800

    创建工程

---
+++ Code/KanZhiHu/KanZhiHu/AppDelegate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KanZhiHu
+//
+//  Created by Sam Lau on 9/28/16.
+//  Copyright © 2016 Sam Lau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Code/KanZhiHu/Pods/Target Support Files/Pods-KanZhiHu/Pods-KanZhiHu-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_KanZhiHu : NSObject
+@end
+@implementation PodsDummy_Pods_KanZhiHu
+@end
